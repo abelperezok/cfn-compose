3798d99ce7487942b4eac56f9bd50ed73eee0bc5
Abel Perez Martinez <abel@ABEL-DESKTOP>
2018-09-30 21:20:00 +0100

    Added version option

--- src/cfn-compose/Program.cs
+++ src/cfn-compose/Program.cs
@@ -1,16 +1,20 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
 using System.IO;
+using System.Reflection;
 using McMaster.Extensions.CommandLineUtils;
 
 namespace cfn_compose
 {
     class Program
     {
+        [Option(ShortName = "V")]
+        public bool Version { get; set; }
+
         [Option(ShortName = "v")]
         public bool Verbose { get; set; }
 
-        [Required]
+        [FileExists]
         [Argument(0, "input", "input file name")]
         public string Input { get; }
 
@@ -24,10 +28,24 @@ namespace cfn_compose
 
         private void OnExecute()
         {
+            if (Version)
+            {
+                var assembly = Assembly.GetExecutingAssembly();
+                var assemblyVersion = assembly.GetName().Version;
+                Console.WriteLine($"AssemblyVersion {assemblyVersion}");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(Input))
+            {
+                Console.WriteLine("Input argument is required");
+                return;
+            }
+
             var directory = Path.GetDirectoryName(Input);
 
             if (Verbose)
-                Console.WriteLine("Using search directory: " + directory);
+                Console.WriteLine($"Using search directory: {directory}");
 
             var input = File.ReadAllText(Input);
 
